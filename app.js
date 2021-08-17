23b13d7dd141765d8320c71128740a1d8c944df0
El Padlo <nazar.hromadskyi@gmail.com>
2021-08-17 13:31:09 +0300

    read json files from 1800

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
 const fs = require('fs');
 const path = require('path');
 
-
 const taskPath = path.join(__dirname, 'taskDir');
+const femaleGroup = path.join(taskPath, '1800');
+const maleGroup = path.join(taskPath, '2000');
 
-fs.readdir(taskPath, (err, files) => {
+// console.log(femaleGroup, 'female group');
+// console.log(maleGroup, 'male group');
+
+
+fs.readdir(femaleGroup, (err, files) => {
     if (err) {
         console.log(err);
         return;
     }
 
-    console.log(files);
+
+    files.forEach(file => {
+        fs.readFile(path.join(femaleGroup, file), (err1, data) => {
+            if (err1) {
+                console.log(err1);
+                return;
+            }
+
+            console.log(JSON.parse(data.toString()));
+        })
+    });
+
 });
