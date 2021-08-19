d9b4d871e9e90f674491d99f07b315dad79b02a5
El Padlo <nazar.hromadskyi@gmail.com>
2021-08-19 17:58:53 +0300

    LogIn

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const expressHbs = require('express-handlebars');
 const path = require('path');
+const fs = require('fs');
 
 const users = require('./db/users');
 
 const app = express();
 
+
 const staticPath = path.join(__dirname, 'static');
+const usersPath = path.join(__dirname, 'db', 'users.js');
 
 
 app.use(express.json());
@@ -19,13 +22,44 @@ app.set('views', staticPath);
 
 
 app.get('/', (req, res) => {
-    res.status(404).end('Not Found');
-})
+    res.status(200).end('welcome');
+});
 
+
+/*-----------------------------------------LOG--------------------------------------------------------------------*/
 app.get('/login', (req, res) => {
     res.render('login');
-})
+});
 
+app.post('/login', (req, res) => {
+    fs.readFile(usersPath, (err, data) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+
+        const {email, password} = req.body;
+
+        for (const user of users) {
+            if (user.email === email && user.password === password) {
+                res.redirect('/users');
+            }
+        }
+
+
+        res.redirect('/registration');
+
+    });
+});
+/*----------------------------------------------------------------------------------------------------------------*/
+
+/*-----------------------------------------REG--------------------------------------------------------------------*/
+app.get('/registration', (req, res) => {
+    res.render('registration');
+})
+/*----------------------------------------------------------------------------------------------------------------*/
+
+/*-----------------------------------------USERS------------------------------------------------------------------*/
 app.get('/users', (req, res) => {
     res.render('users', {users});
 });
@@ -35,23 +69,18 @@ app.get('/users/:user_id', (req, res) => {
     const currentUser = users[user_id];
 
     if (!currentUser) {
-        res.status(404).end('User Not Found');
+        res.status(404).end(`User with id "${user_id}" Not Found`);
         return;
     }
 
 
     res.json(currentUser);
-})
-
-
-app.post('/auth', (req, res) => {
-    console.log(req.body);
-    res.json('OK');
-})
+});
 
 
 app.listen(5000, () => {
     console.log('App listen 5000');
 });
+/*----------------------------------------------------------------------------------------------------------------*/
 
 
