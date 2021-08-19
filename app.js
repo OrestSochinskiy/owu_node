7b273ac7c84dea2d5edb3b4209344011cf5fadf6
El Padlo <nazar.hromadskyi@gmail.com>
2021-08-19 16:49:38 +0300

    lection sandbox

--- app.js
+++ app.js
@@ -1,1 +1,57 @@
 const express = require('express');
+const expressHbs = require('express-handlebars');
+const path = require('path');
+
+const users = require('./db/users');
+
+const app = express();
+
+const staticPath = path.join(__dirname, 'static');
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use(express.static(staticPath));
+app.set('view engine', '.hbs');
+app.engine('.hbs', expressHbs({defaultLayout: false}));
+app.set('views', staticPath);
+
+
+app.get('/', (req, res) => {
+    res.status(404).end('Not Found');
+})
+
+app.get('/login', (req, res) => {
+    res.render('login');
+})
+
+app.get('/users', (req, res) => {
+    res.render('users', {users});
+});
+
+app.get('/users/:user_id', (req, res) => {
+    const {user_id} = req.params;
+    const currentUser = users[user_id];
+
+    if (!currentUser) {
+        res.status(404).end('User Not Found');
+        return;
+    }
+
+
+    res.json(currentUser);
+})
+
+
+app.post('/auth', (req, res) => {
+    console.log(req.body);
+    res.json('OK');
+})
+
+
+app.listen(5000, () => {
+    console.log('App listen 5000');
+});
+
+
---
+++ db/users.js
@@ -0,0 +1,6 @@
+module.exports = [
+    {name: 'Max', age: 23, password: 'password'},
+    {name: 'Anna', age: 19, password: 'password'},
+    {name: 'Oleh', age: 26, password: 'password'},
+    {name: 'Vika', age: 26, password: 'password'}
+];
