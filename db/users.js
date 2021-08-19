7cd82d9f574731275779098a6ef357d6e91ce8b3
El Padlo <nazar.hromadskyi@gmail.com>
2021-08-19 16:57:44 +0300

    added new users

--- db/users.js
+++ db/users.js
@@ -1,6 +1,8 @@
 module.exports = [
-    {name: 'Max', age: 23, password: 'password'},
-    {name: 'Anna', age: 19, password: 'password'},
-    {name: 'Oleh', age: 26, password: 'password'},
-    {name: 'Vika', age: 26, password: 'password'}
+    {name: 'Max', age: 23, email: 'max@email.com', password: 'password'},
+    {name: 'Anna', age: 19, email: 'anna@email.com', password: 'password'},
+    {name: 'Oleh', age: 26, email: 'oleh@email.com', password: 'password'},
+    {name: 'Vika', age: 24, email: 'vika@email.com', password: 'password'},
+    {name: 'Alex', age: 56, email: 'alex@email.com', password: 'password'},
+    {name: 'Nastia', age: 215, email: 'nastia@email.com', password: 'password'}
 ];
