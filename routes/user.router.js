9adae1e424948602750d6bd5f97ae5d13c253c28
El Padlo <nazar.hromadskyi@gmail.com>
2021-08-21 15:14:20 +0300

    test routes

--- routes/user.router.js
+++ routes/user.router.js
@@ -1,5 +1,9 @@
 const router = require('express').Router();
 
+const { userController } = require('../controllers');
+
+router.post('/', userController.createUser);
+
 router.get('/', (req, res) => {
     res.json('HELLO');
 });
