693b124c7cbbbce8944d0affdefc36373d3c87cc
El Padlo <nazar.hromadskyi@gmail.com>
2021-08-21 15:51:19 +0300

    created

---
+++ services/user.service.js
@@ -0,0 +1,10 @@
+// const fs = require('fs');
+// const path = require('path');
+
+const users = require('../db/users');
+
+// const usersPath = path.join(__dirname, 'db', 'users');
+
+module.exports = {
+    findAllUsers: () => users
+};
