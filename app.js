2f7cb94cf6fc5f6d922c6acb56fd0f8d0c0929e4
El Padlo <nazar.hromadskyi@gmail.com>
2021-08-21 12:47:18 +0300

    code refactored

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path');
 const fs = require('fs');
 
 const users = require('./db/users');
-const {PORT} = require('./configs/variables');
+const { PORT } = require('./configs/variables');
 
 const staticPath = path.join(__dirname, 'static');
 const usersPath = path.join(__dirname, 'db', 'users.js');
@@ -12,25 +12,25 @@ const usersPath = path.join(__dirname, 'db', 'users.js');
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(staticPath));
 app.set('view engine', '.hbs');
-app.engine('.hbs', expressHbs({defaultLayout: false}));
+app.engine('.hbs', expressHbs({ defaultLayout: false }));
 app.set('views', staticPath);
 
 app.get('/', (req, res) => {
     res.render('welcome');
 });
 
-/*-----------------------------------------LOG--------------------------------------------------------------------*/
+/* -----------------------------------------LOG--------------------------------------------------------------------*/
 app.get('/login', (req, res) => {
-    res.render('login', {users});
+    res.render('login', { users });
 });
 
 app.post('/login', (req, res) => {
-    const {email, password} = req.body;
-    const indexOfUser = users.findIndex(user => user.email === email && user.password === password);
+    const { email, password } = req.body;
+    const indexOfUser = users.findIndex((user) => user.email === email && user.password === password);
 
     if (indexOfUser === -1) {
         res.redirect('/registration');
@@ -41,19 +41,19 @@ app.post('/login', (req, res) => {
 });
 /*----------------------------------------------------------------------------------------------------------------*/
 
-/*-----------------------------------------REG--------------------------------------------------------------------*/
+/* -----------------------------------------REG--------------------------------------------------------------------*/
 app.get('/registration', (req, res) => {
     res.render('registration');
 });
 
 app.post('/users', (req, res) => {
-    const {email} = req.body;
-    const isFind = users.find(user => user.email === email);
+    const { email } = req.body;
+    const isFind = users.find((user) => user.email === email);
 
     if (!isFind) {
         users.push(req.body);
 
-        fs.writeFile(usersPath, `module.exports = ${JSON.stringify(users)}`, err1 => {
+        fs.writeFile(usersPath, `module.exports = ${JSON.stringify(users)}`, (err1) => {
             if (err1) {
                 console.log(err1);
             }
@@ -66,19 +66,18 @@ app.post('/users', (req, res) => {
     res.redirect('/error');
 });
 
-
 app.get('/error', (req, res) => {
     res.render('error');
 });
 /*----------------------------------------------------------------------------------------------------------------*/
 
-/*-----------------------------------------USERS------------------------------------------------------------------*/
+/* -----------------------------------------USERS------------------------------------------------------------------*/
 app.get('/users', (req, res) => {
-    res.render('users', {users});
+    res.render('users', { users });
 });
 
 app.get('/users/:user_id', (req, res) => {
-    const {user_id} = req.params;
+    const { user_id } = req.params;
     const currentUser = users[user_id];
 
     if (!currentUser) {
@@ -86,13 +85,10 @@ app.get('/users/:user_id', (req, res) => {
         return;
     }
 
-    res.render('singleUser', {currentUser});
+    res.render('singleUser', { currentUser });
 });
-
 
 /*----------------------------------------------------------------------------------------------------------------*/
 app.listen(PORT, () => {
     console.log('App listen 5000');
 });
-
-
