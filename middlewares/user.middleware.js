bd31dfe5989e0736faddf2afb22cb22e69b8ad00
El Padlo <nazar.hromadskyi@gmail.com>
2021-08-26 14:15:00 +0300

    is email taken check

--- middlewares/user.middleware.js
+++ middlewares/user.middleware.js
@@ -1,5 +1,6 @@
 const User = require('../dataBase/User');
 const ErrorHandler = require('../errors/ErrorHandler');
+const { findAllUsers } = require('../services/user.service');
 
 module.exports = {
     isUserExist: async (req, res, next) => {
@@ -15,6 +16,22 @@ module.exports = {
             next();
         } catch (e) {
             next(e);
+        }
+    },
+
+    isEmailUsed: async (req, res, next) => {
+        try {
+            const { email } = req.body;
+
+            const isEmailFound = await findAllUsers({ email });
+
+            if (isEmailFound.length) {
+                throw new ErrorHandler(409, `${email} -- is already in use`);
+            }
+
+            next();
+        } catch (e) {
+            next();
         }
     }
 };
