892a42704a872ecfbd4407b82f217277af2734a1
El Padlo <nazar.hromadskyi@gmail.com>
2021-08-26 16:12:43 +0300

    create, delete car

--- services/car.service.js
+++ services/car.service.js
@@ -1,5 +1,9 @@
 const Car = require('../dataBase/Car');
 
 module.exports = {
-    findAllCars: (filter) => Car.find(filter)
+    findAllCars: (filter) => Car.find(filter),
+
+    createCar: (car) => Car.create(car),
+
+    deleteCar: (carId) => Car.findByIdAndDelete(carId)
 };
