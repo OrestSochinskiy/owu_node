e2bd57373729771039f960778fc1a42811bb9ad7
El Padlo <nazar.hromadskyi@gmail.com>
2021-08-26 16:19:22 +0300

    crud for car

--- controllers/car.controller.js
+++ controllers/car.controller.js
@@ -1,6 +1,10 @@
 const carService = require('../services/car.service');
 
-const { findAllCars } = carService;
+const {
+    findAllCars,
+    createCar,
+    deleteCar
+} = carService;
 
 module.exports = {
     getAllCars: async (req, res, next) => {
@@ -8,6 +12,35 @@ module.exports = {
             const allCars = await findAllCars();
 
             res.json(allCars);
+        } catch (e) {
+            next(e);
+        }
+    },
+
+    getCarById: (req, res, next) => {
+        try {
+            res.json(req.car);
+        } catch (e) {
+            next(e);
+        }
+    },
+
+    createCar: async (req, res, next) => {
+        try {
+            const createdCar = await createCar(req.body);
+
+            res.json(createdCar);
+        } catch (e) {
+            next(e);
+        }
+    },
+
+    deleteCar: async (req, res, next) => {
+        try {
+            const { car } = req;
+            await deleteCar(car._id);
+
+            res.json('deleted');
         } catch (e) {
             next(e);
         }
--- routes/car.routes.js
+++ routes/car.routes.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 
 const { carController } = require('../controllers');
+const { isCarExist } = require('../middlewares/car.middleware');
 
 router.get('/', carController.getAllCars);
+router.post('/', carController.createCar);
+router.get('/:car_id', isCarExist, carController.getCarById);
+router.delete('/:car_id', isCarExist, carController.deleteCar);
 
 module.exports = router;
