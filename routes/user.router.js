5715bfd09b30abb6d30c0a0b33f9a326af3107e4
El Padlo <nazar.hromadskyi@gmail.com>
2021-08-26 14:16:30 +0300

    is email used middleware

--- routes/user.router.js
+++ routes/user.router.js
@@ -1,12 +1,12 @@
 const router = require('express').Router();
 
 const { userController } = require('../controllers');
-const { isUserExist } = require('../middlewares/user.middleware');
+const { isUserExist, isEmailUsed } = require('../middlewares/user.middleware');
 
 router.get('/', userController.getAllUsers);
-router.post('/', userController.createUser);
+router.post('/', isEmailUsed, userController.createUser);
 router.get('/:user_id', isUserExist, userController.getUserById);
-router.delete('/:user_id', userController.deleteUser);
+router.delete('/:user_id', isUserExist, userController.deleteUser);
 // router.get('/:user_name', userController.getUserByName);
 
 module.exports = router;
