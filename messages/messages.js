8318a6f0b18f535fda5e0f43d32b4374242d2fa7
El Padlo <nazar.hromadskyi@gmail.com>
2021-08-26 17:55:58 +0300

    const UPDATED

--- messages/messages.js
+++ messages/messages.js
@@ -3,5 +3,6 @@ module.exports = {
     CAR_NOT_FOUND: 'Car not found',
     EMPTY_FIELDS: 'Fields are empty',
     INCORRECT_PRICE: 'Incorrect price',
-    ALREADY_EXIST: 'Already exist'
+    ALREADY_EXIST: 'Already exist',
+    UPDATED: 'Updated'
 };
